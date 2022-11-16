4af2b5f595cef3881d0895b018e640a32e648001
Uzariustics <102352925+uzariustics123@users.noreply.github.com>
2022-11-16 23:21:05 +0800

    fix and organize evaluation view page
    organize the data display by separating questions with cards and table

--- analytics/evaluations.php
+++ analytics/evaluations.php
@@ -25,42 +25,112 @@
                     $event_id = $_GET['event_id'];
                     ?>
 
-                    <div class="d-flex align-items-center">
+                    <div class="d-flex flex-column">
+                        <!-- <table class="table dt-responsive nowrap w-100"> -->
                         <?php
                         $query1 = "SELECT * FROM questions WHERE event_reference_id = $event_id;";
                         $results1 = $conn->query($query1);
+                        $questionArray = array();
                         while ($row = $results1->fetch_assoc()) {
+                            $questionArray[] = $row;
                             $question_id = $row['question_id'];
-                        ?><div>
-                                <div class="border border-bottom-0 flex-grow-1 q" data-c="<?= $row['question_content'] ?>" data-q="<?= $row['question_id']; ?>" data-e="<?= $event_id; ?>"></div>
-                            </div>
-                        <?php } ?>
-                    </div>
+                            $question_type = $row['question_type'];
 
-                    <div class="d-flex align-items-center">
-                        <?php
-                        $results1 = $conn->query($query1);
-                        while ($row1 = $results1->fetch_assoc()) {
-                            echo '<div class="border flex-grow-1" id="' . $row1['question_id'] . '"></div>';
-                        }
-
-                        $query = "SELECT * FROM evaluations WHERE event_reference_id = $event_id";
-                        $results = $conn->query($query);
-                        while ($row1 = $results->fetch_assoc()) {
-                            $data[] = $row1;
-                        }
-
-                        $dataJSON = json_encode($data);
                         ?>
+                            <div class="card p-1">
+                                <th>
+                                    <h5 class="card-title"><?= $row['question_content'] ?></h5>
+                                </th>
+                                <?php
+                                if ($question_type == 'rating') {
+                                    $very_satisfied = 0;
+                                    $satisfy = 0;
+                                    $fair = 0;
+                                    $poor = 0;
+                                    $very_poor = 0;
+                                    $Qresponse = "SELECT * FROM evaluations WHERE event_reference_id = $event_id AND question_reference_id = $question_id";
+                                    $results = $conn->query($Qresponse);
+                                ?>
+                                    <table class="table dt-responsive nowrap w-100">
+                                        <thead>
+                                            <tr>
+                                                <th>Very Satisfied</th>
+                                                <th>Satisfied</th>
+                                                <th>Fair</th>
+                                                <th>Poor</th>
+                                                <th>Very Poor</th>
+                                            </tr>
+                                        </thead>
+
+                                        <?php
+                                        while ($row1 = $results->fetch_assoc()) {
+                                            // $data[] = $row1;
+                                            if (strtolower($row1['evaluation_content']) == 'very satisfied') {
+                                                $very_satisfied++;
+                                            } else if (strtolower($row1['evaluation_content']) == 'satisfied') {
+                                                $satisfy++;
+                                            } else if (strtolower($row1['evaluation_content']) == 'fair') {
+                                                $fair++;
+                                            } else if (strtolower($row1['evaluation_content']) == 'poor') {
+                                                $poor++;
+                                            } else if (strtolower($row1['evaluation_content']) == 'very Poor') {
+                                                $very_poor++;
+                                            }
+                                        }
+                                        ?>
+                                        <tbody>
+                                            <tr>
+                                                <td><?= $very_satisfied ?> responded</td>
+                                                <td><?= $satisfy ?> responded</td>
+                                                <td><?= $fair ?> responded</td>
+                                                <td><?= $poor ?> responded</td>
+                                                <td><?= $very_poor ?> responded</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                <?php
+                                } else {
+                                    $Qresponse = "SELECT * FROM evaluations WHERE event_reference_id = $event_id AND question_reference_id = $question_id";
+                                    $results = $conn->query($Qresponse);
+                                ?><ol class="list-group list-group-numbered">
+
+                                        <?php
+                                        while ($row1 = $results->fetch_assoc()) {
+                                            // $data[] = $row1;
+                                        ?><li class="list-group-item"><?= $row1['evaluation_content'] ?></li>
+                                        <?php
+                                        }
+                                        ?>
+                                    </ol> <?php
+                                        }
+                                            ?>
+                            </div>
+                        <?php
+                        } ?>
+
+                        <!-- </table> -->
+
                     </div>
-
-
-
-
                 </div>
-            </div>
 
+                <div class="d-flex align-items-center">
+                    <?php
+                    // $results1 = $conn->query($query1);
+
+
+
+
+                    // $dataJSON = json_encode($data);
+                    ?>
+                </div>
+
+
+
+
+            </div>
         </div>
+
+    </div>
     </div>
     <!-- END wrapper -->
     <?php require_once '../includes/endbar.php' ?>
@@ -77,22 +147,22 @@
 
 
 
-        $('.q').each(function() {
-            let q_id = $(this).attr('data-q');
-            let c = $(this).attr('data-c');
+        // $('.q').each(function() {
+        //     let q_id = $(this).attr('data-q');
+        //     let c = $(this).attr('data-c');
 
-            const evaluations = data.filter(d => d.question_reference_id === q_id);
-            console.log(evaluations)
+        //     const evaluations = data.filter(d => d.question_reference_id === q_id);
+        //     console.log(evaluations)
 
-            let row = '<table class="table table-border">';
-            row += `<tr><td class="fw-bold text-center h4">${c}</td></tr>`;
-            evaluations.forEach(e => {
-                row += `<tr><td class="text-center">${e.evaluation_content}</td></tr>`;
-            });
-            row += `</table>`;
-            $(`#${q_id}`).append(row);
-            row = '';
-        });
+        //     let row = '<table class="table table-border">';
+        //     row += `<tr><td class="fw-bold text-center h4">${c}</td></tr>`;
+        //     evaluations.forEach(e => {
+        //         row += `<tr><td class="text-center">${e.evaluation_content}</td></tr>`;
+        //     });
+        //     row += `</table>`;
+        //     $(`#${q_id}`).append(row);
+        //     row = '';
+        // });
 
 
         console.log(table);
